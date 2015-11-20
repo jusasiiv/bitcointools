cf4c9b0c94f44c7f2fefa92dc0c031c3d79e04bd
Shiva S <shivaenigma@gmail.com>
2015-11-20 13:12:50 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='bitcointools',
+    version='0.1',
+    description='Bitcoin tools utility',
+    author='GavinAndresen',
+    author_email='<gavinandresen@gmail.com>',
+    maintainer='Shiva S',
+    maintainer_email='<shivaenigma@gmail.com>',
+    url='https://github.com/shivaenigma/bitcointools',
+    packages=['bitcointools'],
+    classifiers=[
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)', 'Operating System :: OS Independent'
+    ]
+)
